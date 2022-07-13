c6932619f1db1af191ed4de88ec9b19aad772897
Sylvain Le Groux <slegroux@ccrma.stanford.edu>
2022-07-13 05:29:45 +0000

    add examples

---
+++ tutorials/tts/egs/hifi_tts_6097.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+export OMP_NUM_THREADS=1
+set -x
+set -a
+
+# DATASET
+data_root=$DATA/en
+dataset=${data_root}/6097_5_mins
+wandb_project_name=$(basename ${dataset})
+exp_dir=${wandb_project_name}
+manifest_filepath=${dataset}/manifest.json
+output_dir=${dataset}
+# TRAIN/TEST FASTPITCH
+train_manifest=${output_dir}/manifest.train.json
+test_manifest=${output_dir}/manifest.test.json
+# TRAIN/TEST HIFIGAN
+train_manifest_hifigan=${output_dir}/manifest.train.hifigan.json
+test_manifest_hifigan=${output_dir}/manifest.test.hifigan.json
+# fine_tune_bs=64
+# n_speakers=1
+
+#./en.sh $ARGS "$@"
+# train/test split
+# time ./en.sh 100
+echo "[INFO] fastpitch fine-tune"
+time ./en.sh 4
+echo "[INFO] hifigan data prep"
+time ./en.sh 6
+echo "[INFO] hifigan fine-tune"
+time ./en.sh 7
+echo "[INFO] test fine-tuned models"
+synth_text="She put a scarf"
+specgen_ckpt_dir=${exp_dir}_finetune
+time ./en.sh 9
